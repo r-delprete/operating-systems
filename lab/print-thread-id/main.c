2f883625fa7e605d75bfb1ed4beff1acc4723139
Rocco Del Prete <122788299+roccodelprete@users.noreply.github.com>
2024-09-23 12:32:28 +0200

    add print thread id exercise

---
+++ lab/print-thread-id/main.c
@@ -0,0 +1,35 @@
+#include "../lib/apue.h"
+#include <pthread.h>
+
+pthread_t threadID;
+
+void printThreadID(const char *msg)
+{
+  pid_t processID = getpid();
+  pthread_t tID = pthread_self();
+
+  printf("%s\t process ID = %lu\t thread ID = %lu\t (0x&lx)\n", msg, processID, (unsigned long)tID, (unsigned long)tID);
+}
+
+void *threadRoutine(void *args)
+{
+  printThreadID("New thread: ");
+
+  return ((void *)0);
+}
+
+int main()
+{
+  int err;
+
+  if ((err = pthread_create(&threadID, NULL, threadRoutine, NULL)) != 0)
+  {
+    fprintf(stderr, "Error creating thread: %s", strerror(err));
+    return -1;
+  }
+
+  printThreadID("Main thread: \t");
+  sleep(1);
+
+  return 0;
+}
