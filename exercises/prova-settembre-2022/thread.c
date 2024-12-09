bd5f705eb39931571ddf3ba59344158be16c1667
Rocco Del Prete <122788299+roccodelprete@users.noreply.github.com>
2024-12-09 18:21:38 +0100

    fix thread

--- exercises/prova-settembre-2022/thread.c
+++ exercises/prova-settembre-2022/thread.c
@@ -1,6 +1,6 @@
 /**
- * Si realizzi un programma in Ce Posix sotto Linux che, con l'ausilio della libreria Pthread,
- * lancia k thread per calcolare il prodotto di due matrici di dimensione kxm e mxp.
+ * Si realizzi un programma in C e Posix sotto Linux che, con l'ausilio della libreria Pthread,
+ * lancia k thread per calcolare il prodotto di due matrici di dimensione k × m e m × p.
  * Non appena sarà calcolata la matrice prodotto, un k+1-esimo thread aggiuntivo, che era rimasto in attesa,
  * provvederà a stampare la matrice risultato. Le matrici devono essere allocate dinamicamente.
  * Usare le variabili di condizione.
@@ -13,23 +13,10 @@
 #include <time.h>
 #include <stdbool.h>
 
-#define NUM_THREADS 8
-#define M 5
-#define P 4
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
-int **firstMatrix, **secondMatrix, **resultMatrix, productsCounter = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-int lock(pthread_mutex_t mutex)
-{
-  return pthread_mutex_lock(&mutex);
-}
-
-int unlock(pthread_mutex_t mutex)
-{
-  return pthread_mutex_unlock(&mutex);
-}
+int **firstMatrix, **secondMatrix, **resultMatrix, productsDone = 0, k, m, p;
 
 int **matrixGeneration(int rows, int cols, bool hasToFill)
 {
@@ -66,47 +53,6 @@ void printMatrix(int **matrix, int rows, int cols)
   }
 }
 
-void *matricesProductRoutine(void *args)
-{
-  int threadID = *((int *)args);
-  free(args);
-
-  lock(mutex);
-  for (int i = 0; i < P; i++)
-  {
-    for (int j = 0; j < M; j++)
-    {
-      resultMatrix[threadID][i] += firstMatrix[threadID][j] * secondMatrix[j][i];
-      productsCounter++;
-    }
-  }
-
-  if (productsCounter == (NUM_THREADS * M * P))
-  {
-    pthread_cond_signal(&condition);
-  }
-
-  unlock(mutex);
-
-  return NULL;
-}
-
-void *printResultMatrixRoutine(void *args)
-{
-  lock(mutex);
-  while (productsCounter != (NUM_THREADS * M * P))
-  {
-    pthread_cond_wait(&condition, &mutex);
-  }
-
-  printf("Result Matrix\n");
-  printMatrix(resultMatrix, NUM_THREADS, P);
-
-  unlock(mutex);
-
-  return NULL;
-}
-
 void matrixDeallocation(int **matrix, int rows)
 {
   for (int i = 0; i < rows; i++)
@@ -116,42 +62,91 @@ void matrixDeallocation(int **matrix, int rows)
   free(matrix);
 }
 
+void *matricesProductRoutine(void *args)
+{
+  int threadID = *((int *)args);
+  free(args);
+
+  pthread_mutex_lock(&mutex);
+  for (int j = 0; j < p; j++)
+  {
+    for (int i = 0; i < m; i++)
+    {
+      resultMatrix[threadID][j] += firstMatrix[threadID][i] * secondMatrix[i][j];
+    }
+  }
+  productsDone++;
+
+  if (productsDone == k)
+  {
+    pthread_cond_signal(&condition);
+  }
+  pthread_mutex_unlock(&mutex);
+
+  return NULL;
+}
+
+void *printResultMatrixRoutine(void *args)
+{
+  pthread_mutex_lock(&mutex);
+  while (productsDone < k)
+  {
+    pthread_cond_wait(&condition, &mutex);
+  }
+
+  printf("\nResult matrix\n");
+  printMatrix(resultMatrix, k, p);
+
+  pthread_mutex_unlock(&mutex);
+
+  return NULL;
+}
+
 int main(int argc, char **argv)
 {
   srand(time(NULL));
 
-  pthread_t *threads = malloc((NUM_THREADS + 1) * sizeof(pthread_t));
-  firstMatrix = matrixGeneration(NUM_THREADS, M, true);
-  secondMatrix = matrixGeneration(M, P, true);
-  resultMatrix = matrixGeneration(NUM_THREADS, P, false);
+  if (argc != 4)
+  {
+    printf("Error! Correct usage: ./<filename> <k value> <m value> <p value>\n");
+    return EXIT_FAILURE;
+  }
+
+  k = atoi(argv[1]);
+  m = atoi(argv[2]);
+  p = atoi(argv[3]);
+
+  firstMatrix = matrixGeneration(k, m, true);
+  secondMatrix = matrixGeneration(m, p, true);
+  resultMatrix = matrixGeneration(k, p, false);
 
   printf("First matrix\n");
-  printMatrix(firstMatrix, NUM_THREADS, M);
-  printf("\n");
+  printMatrix(firstMatrix, k, m);
 
-  printf("Second matrix\n");
-  printMatrix(secondMatrix, M, P);
-  printf("\n");
+  printf("\nSecond matrix\n");
+  printMatrix(secondMatrix, m, p);
 
-  for (int i = 0; i < NUM_THREADS; i++)
+  pthread_t *threads = malloc((k + 1) * sizeof(pthread_t));
+
+  pthread_create(&threads[k], NULL, printResultMatrixRoutine, NULL);
+
+  for (int i = 0; i < k; i++)
   {
     int *threadID = malloc(sizeof(int));
     *threadID = i;
     pthread_create(&threads[i], NULL, matricesProductRoutine, threadID);
   }
 
-  pthread_create(&threads[NUM_THREADS], NULL, printResultMatrixRoutine, NULL);
+  pthread_join(threads[k], NULL);
 
-  for (int i = 0; i < NUM_THREADS; i++)
+  for (int i = 0; i < k; i++)
   {
     pthread_join(threads[i], NULL);
   }
 
-  pthread_join(threads[NUM_THREADS], NULL);
-
-  matrixDeallocation(firstMatrix, NUM_THREADS);
-  matrixDeallocation(secondMatrix, M);
-  matrixDeallocation(resultMatrix, NUM_THREADS);
+  matrixDeallocation(firstMatrix, k);
+  matrixDeallocation(secondMatrix, m);
+  matrixDeallocation(resultMatrix, k);
   free(threads);
 
   return EXIT_SUCCESS;
